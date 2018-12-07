83cfcec9f3b983d3069bf691f138ee79607c3319
294945339@qq.com <86551056>
2018-12-07 15:07:30 +0800

    增加打war包xml

--- admin/src/main/java/io/fast/AdminApplication.java
+++ admin/src/main/java/io/fast/AdminApplication.java
@@ -3,12 +3,19 @@ package io.fast;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"io.fast.modules.*.dao"})
-public class AdminApplication {
+public class AdminApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminApplication.class);
     }
 }
